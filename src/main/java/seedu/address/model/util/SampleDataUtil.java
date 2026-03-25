a730f67ae6f6ff1c3e6579a12edd4e15b14adda8
HoneyGlazedPorkChops <eugeneong3@gmail.com>
2026-03-25 21:36:54 +0800

    Change Delivery class to have a reference to an existing Company instance
    - Rework Delivery model and Delivery class
    - Change AddCommand and EditCommand for Delivery such that it always references a Company instance
    - Edit SampleData to fit context of Company and Delivery
    - Edit Parser to accomodate for Company reference rather than passing a string
    - Rework storage so that saved data is loaded properly with Deliveries having the correct Company reference

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -5,20 +5,30 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.model.AddressBook;
+import seedu.address.model.DeliveryBook;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyDeliveryBook;
 import seedu.address.model.company.Address;
 import seedu.address.model.company.Company;
 import seedu.address.model.company.Email;
 import seedu.address.model.company.Name;
 import seedu.address.model.company.Phone;
+import seedu.address.model.delivery.Address;
+import seedu.address.model.delivery.Delivery;
+import seedu.address.model.delivery.Product;
 import seedu.address.model.tag.Tag;
 
 /**
- * Contains utility methods for populating {@code AddressBook} with sample data.
+ * Contains utility methods for populating {@code AddressBook} and {@code DeliveryBook} with sample data.
  */
-public class SampleAddressDataUtil {
-    public static Company[] getSampleCompanies() {
-        return new Company[] {
+public class SampleDataUtil {
+    public static SampleData getSampleDatas() {
+        Company c1 = new Company(new Name("Apple"), new Phone("87438807"), new Email("apple@example.com"),
+                new seedu.address.model.company.Address("Blk 30 Geylang Street 29, #06-40"),
+                getTagSet("important"));
+        Company c2 = new Company(new Name("Dell"), new Phone("99272758"), new Email("dell@example.com"),
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), getTagSet(""));
+        new Company[] {
             new Company(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
                 getTagSet("friends")),
@@ -38,11 +48,16 @@ public class SampleAddressDataUtil {
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
                 getTagSet("colleagues"))
         };
+
+        Delivery[] deliveries = new Delivery[] {
+                new Delivery(new Product("laptop"), c1, new seedu.address.model.delivery.Address("Blk 30 Geylang Street 29, #06-40"),
+                        getTagSet("fragile"))
+        };
     }
 
     public static ReadOnlyAddressBook getSampleAddressBook() {
         AddressBook sampleAb = new AddressBook();
-        for (Company sampleCompany : getSampleCompanies()) {
+        for (Company sampleCompany : getCompanies()) {
             sampleAb.addCompany(sampleCompany);
         }
         return sampleAb;
